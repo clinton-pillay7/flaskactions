b107f94dc9e5e505b7328bf431d5be9b1764d8ec
EC2 Default User <ec2-user@ip-172-31-8-120.ec2.internal>
2026-02-26 20:54:30 +0000

    Test pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, universe!"
 
 if __name__ == "__main__":
     # host="0.0.0.0" makes it accessible on the machine's IP
