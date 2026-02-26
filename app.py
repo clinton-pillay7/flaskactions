15e630bc02b398112ece33afbaae76e1c8ccf1bb
EC2 Default User <ec2-user@ip-172-31-8-120.ec2.internal>
2026-02-26 20:58:41 +0000

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, universe!"
+    return "Hello, earth!"
 
 if __name__ == "__main__":
     # host="0.0.0.0" makes it accessible on the machine's IP
