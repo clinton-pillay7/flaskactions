22612c1221325af5c506cb79608a77ebc6fc4756
Clinton <clinton.pillay@outlook.com>
2026-03-02 22:13:22 +0200

    Final changes for release

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello earth!!!! - see what SSH did"
+    return "Hello earth!!!! - see what SSH did - with versioning this time"
 
 if __name__ == "__main__":
     # host="0.0.0.0" makes it accessible on the machine's IP
