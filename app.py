3d94f5416df0f3e1fdef28027e4cd634af156c56
Clinton <clinton.pillay@outlook.com>
2026-03-11 23:05:03 +0200

    JIRA-2: jira intergration v3

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """ Home Function """
-    return "Hello earth!!!! - see what SSH did - with versioning this time and Jira intergration"
+    return "Hello earth!!!! - see what SSH did - with versioning this time and Jira intergration v3"
 
 
 
